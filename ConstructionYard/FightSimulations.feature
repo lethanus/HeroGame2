72143839486a8c528fd456c1c12bd57e136f2372
Emil <emil.drozdowski@LAPTOP-CR7CEFSM>
2018-11-06 17:19:44 +0100

    InitialTest

---
+++ ConstructionYard/FightSimulations.feature
@@ -0,0 +1,16 @@
+﻿Feature: FightSimulations
+	Fighting 
+
+@mytag
+Scenario: Elf hits Goblin
+	Given The following characters
+		| Name   | MaxHp | Hp | Att | Def | Speed |
+		| Elf    | 20    | 20 | 10  | 5   | 10    |
+		| Golbin | 10    | 10 | 10  | 0   | 5     |
+	And Character 'Elf' is assigned to team 'A'
+	And Character 'Goblin' is assigned to team 'B'
+	When Fight turn 1 ends
+	Then The following characters status is
+		| Name   | MaxHp | Hp | Att | Def | Speed |
+		| Elf    | 20    | 20 | 10  | 5   | 10    |
+		| Golbin | 10    | 0  | 10  | 0   | 5     |
---
+++ FightSimulationsSteps.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+
+namespace ConstructionYard
+{
+    [Binding]
+    public class FightSimulationsSteps
+    {
+        [Given(@"The following characters")]
+        public void GivenTheFollowingCharacters(Table table)
+        {
+            Assert.IsFalse(true);
+        }
+        
+        [Given(@"Character '(.*)' is assigned to team '(.*)'")]
+        public void GivenCharacterIsAssignedToTeam(string p0, string p1)
+        {
+            Assert.IsFalse(true);
+        }
+        
+        [When(@"Fight turn (.*) ends")]
+        public void WhenFightTurnEnds(int p0)
+        {
+            Assert.IsFalse(true);
+        }
+        
+        [Then(@"The following characters status is")]
+        public void ThenTheFollowingCharactersStatusIs(Table table)
+        {
+            Assert.IsFalse(true);
+        }
+    }
+}
