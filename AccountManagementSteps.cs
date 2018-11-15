cdc689e88bd5e6a0a91a38b2b5b3930b7383b0a1
Emil <emil.drozdowski@LAPTOP-CR7CEFSM>
2018-11-15 13:22:31 +0100

    AccountManagementSteps red

---
+++ AccountManagementSteps.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+using TechTalk.SpecFlow.Assist;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConstructionYard
+{
+    [Binding]
+    public class AccountManagementSteps
+    {
+        private List<Account> accounts;
+        private Account loggedId = null;
+
+
+        [Given(@"Some accounts exists in system")]
+        public void GivenSomeAccountsExistsInSystem(Table table)
+        {
+            accounts = table.CreateSet<Account>().ToList();
+            Assert.Greater(accounts.Count, 0);
+        }
+        
+        [When(@"I try to login for '(.*)' and password '(.*)'")]
+        public void WhenITryToLoginForAndPassword(string givenLogin, string givenPassword)
+        {
+            AccountManagement accountManagement = new AccountManagement();
+            loggedId = accountManagement.Login(givenLogin, givenPassword);
+        }
+        
+        [Then(@"Logged account id is '(.*)'")]
+        public void ThenLoggedAccountIdIs(string expectedAccountID)
+        {
+            if (expectedAccountID == "none") Assert.AreEqual(null, loggedId);
+            else Assert.AreEqual(expectedAccountID, loggedId.ID);
+        }
+    }
+
+    public class AccountManagement
+    {
+        public Account Login(string login, string password)
+        {
+            return null;
+        }
+    }
+
+    public class Account
+    {
+        public string ID { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+    }
+}
---
+++ ConstructionYard/AccountManagement.feature
@@ -0,0 +1,17 @@
+﻿Feature: AccountManagement
+	
+
+@mytag
+Scenario: Successful login
+	Given Some accounts exists in system
+	| ID   | Login | Password |
+	| ID_1 | test  | test     |
+	When I try to login for 'test' and password 'test'
+	Then Logged account id is 'ID_1'
+
+Scenario: Failed login
+	Given Some accounts exists in system
+	| ID   | Login | Password |
+	| ID_1 | test  | test     |
+	When I try to login for 'test' and password 'wrong'
+	Then Logged account id is 'none'
