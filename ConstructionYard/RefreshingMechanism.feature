9a3aab35bcfff0b99590c66f7b28baac888f8c9d
Emil <emil.drozdowski@LAPTOP-CR7CEFSM>
2018-11-16 14:31:18 +0100

    RefreshingMechanismSteps - initial

---
+++ ConstructionYard/RefreshingMechanism.feature
@@ -0,0 +1,16 @@
+﻿Feature: RefreshingMechanism
+	This will ensure that player cannot use refresh button until given time passed
+
+@mytag
+Scenario: Refreshing for the first time
+	Given Some accounts exists in system
+	| ID   | Login | Password |
+	| ID_1 | test  | test     |
+	And that there was no refresh actions before for option 'Test' for account ID 'ID_1'
+	And current time is set to '2018-10-15 10:00:00'
+	And Refresh time for option 'Test' is set to '30' seconds
+	When I try to login for 'test' and password 'test'
+	And mechanizm will set refresh to 'Enabled' for option 'Test' for account ID 'ID_1'
+	And player with account ID 'ID_1' will use refresh for 'Test' option at '2018-10-15 10:00:00'
+	Then Logged account id is 'ID_1'
+	And Refresh for option 'Test' is 'Disabled' and next refresh is available at '2018-10-15 10:00:31' for account ID 'ID_1'
---
+++ ConstructionYard/RefreshingMechanismSteps.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace ConstructionYard
+{
+    [Binding]
+    public class RefreshingMechanismSteps
+    {
+        [Given(@"that there was no refresh actions before for option '(.*)' for account ID '(.*)'")]
+        public void GivenThatThereWasNoRefreshActionsBeforeForOption(string option, string accountID)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"current time is set to '(.*)'")]
+        public void GivenCurrentTimeIsSetTo(DateTime currentTime)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"Refresh time for option '(.*)' is set to '(.*)' seconds")]
+        public void GivenRefreshTimeForOptionIsSetToSeconds(string option, int delayInSeconds)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"mechanizm will set refresh to '(.*)' for option '(.*)' for account ID '(.*)'")]
+        public void WhenMechanizmWillSetRefreshToForOption(string status, string option, string accountID)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"player with account ID '(.*)' will use refresh for '(.*)' option at '(.*)'")]
+        public void WhenPlayerWillUseRefreshForOptionAt(string accountID, string option, DateTime actionTime)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Refresh for option '(.*)' is '(.*)' and next refresh is available at '(.*)' for account ID '(.*)'")]
+        public void ThenRefreshForOptionIsAndNextRefreshIsAvailableAt(string option, string status, DateTime nextActionTime, string accountID)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
