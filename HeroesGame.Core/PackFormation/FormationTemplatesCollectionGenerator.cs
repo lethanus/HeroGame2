fec4ce2d2a74afa8cb2a65e4b6d9ddf1a56f900f
Emil <emil.drozdowski@LAPTOP-CR7CEFSM>
2018-12-01 15:20:36 +0100

    Adding more default formations for GUI tests

--- HeroesGame.Core/PackFormation/FormationTemplatesCollectionGenerator.cs
+++ HeroesGame.Core/PackFormation/FormationTemplatesCollectionGenerator.cs
@@ -27,6 +27,64 @@ namespace HeroesGame.PackBuilding
             });
             tempaltes.Add(new FormationTemplate
             {
+                ID = "elf_1",
+                Name = "Elf easy",
+                Level = 1,
+                F1 = "Elf@1",
+                F2 = "Elf@2",
+                F3 = "Elf@1",
+                M1 = "Elf@1",
+                M2 = "Elf@1",
+                M3 = "Elf@2",
+                M4 = "Elf@1"
+            });
+            tempaltes.Add(new FormationTemplate
+            {
+                ID = "ork_1",
+                Name = "Ork easy",
+                Level = 1,
+                F1 = "Ork@1",
+                F2 = "Ork@2",
+                F3 = "Ork@1",
+                M1 = "Ork@1",
+                M2 = "Ork@1",
+                M3 = "Ork@2",
+                M4 = "Ork@1"
+            });
+            tempaltes.Add(new FormationTemplate
+            {
+                ID = "elf_1",
+                Name = "Elf normal",
+                Level = 2,
+                F1 = "Elf@2",
+                F2 = "Elf@2",
+                F3 = "Elf@2",
+                M1 = "Elf@2",
+                M2 = "Elf@2",
+                M3 = "Elf@2",
+                M4 = "Elf@2",
+                R1 = "Elf@2",
+                R2 = "Elf@2",
+                R3 = "Elf@2"
+            });
+            tempaltes.Add(new FormationTemplate
+            {
+                ID = "ork_1",
+                Name = "Ork normal",
+                Level = 2,
+                F1 = "Ork@2",
+                F2 = "Ork@2",
+                F3 = "Ork@2",
+                M1 = "Ork@2",
+                M2 = "Ork@2",
+                M3 = "Ork@2",
+                M4 = "Ork@2",
+                R1 = "Ork@2",
+                R2 = "Ork@2",
+                R3 = "Ork@2"
+            });
+            tempaltes.Add(new FormationTemplate
+            {
                 ID = "goblin_2",
                 Name = "Goblin normal",
                 Level = 2,
@@ -56,6 +114,22 @@ namespace HeroesGame.PackBuilding
                 R1 = "Goblin@3",
                 R2 = "Goblin@3",
                 R3 = "Goblin@3"
+            });
+            tempaltes.Add(new FormationTemplate
+            {
+                ID = "goblin_4",
+                Name = "Goblin very hard",
+                Level = 4,
+                F1 = "Goblin@4",
+                F2 = "Goblin@4",
+                F3 = "Goblin@4",
+                M1 = "Goblin@4",
+                M2 = "Goblin@4",
+                M3 = "Goblin@4",
+                M4 = "Goblin@4",
+                R1 = "Goblin@4",
+                R2 = "Goblin@4",
+                R3 = "Goblin@4"
             });
 
             return tempaltes;
